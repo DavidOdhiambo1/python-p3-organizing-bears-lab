deb7a8a00f782c0db5e59835f6f186b98015b958
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2025-02-04 10:18:00 +0300

    use tableName.columnName syntax

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,19 +1,19 @@
 select_all_female_bears_return_name_and_age = """
-    SELECT name, age FROM bears WHERE sex = "F";
+    SELECT bears.name, bears.age FROM bears WHERE sex = "F";
 """
 
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT name FROM bears ORDER BY name ASC;
+    SELECT bears.name FROM bears ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT name, age FROM bears WHERE alive IS TRUE ORDER BY age;
+    SELECT bears.name, bears.age FROM bears WHERE alive IS TRUE ORDER BY age;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;
+    SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    SELECT name, age FROM bears ORDER BY age LIMIT 1;
+    SELECT bears.name, bears.age FROM bears ORDER BY age LIMIT 1;
 """
